d8e72018d9e5982347a848499ee4c50e4414d124
marksikaundi <emmanuelmark117@gmail.com>
2025-01-05 18:07:05 +0200

    TEST: Doing some samples

---
+++ 01/intro.py
@@ -0,0 +1,10 @@
+# Example of variables
+x = 10          # Integer
+y = 3.14        # Float
+name = "Lupleg"  # String
+is_student = False  # Boolean
+
+print(x)        # Output: 10
+print(y)        # Output: 3.14
+print(name)     # Output: Alice
+print(is_student)  # Output: True
