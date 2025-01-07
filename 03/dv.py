41c9d88adb42364be0f08f254aa6bf515f01b44e
marksikaundi <emmanuelmark117@gmail.com>
2025-01-07 04:52:42 +0200

    FIX: Suppress type checking warnings for matplotlib and seaborn imports in dv.py

--- 03/dv.py
+++ 03/dv.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib.pyplot as plt # type: ignore
+import seaborn as sns # type: ignore
 import numpy as np
 import pandas as pd
 
