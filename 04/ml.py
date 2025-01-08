e8b05e236219db0ff2f09b5265b4314638efc6ef
marksikaundi <emmanuelmark117@gmail.com>
2025-01-08 09:59:48 +0200

    UPDATED: Replace Boston Housing dataset with California Housing dataset in machine learning script

--- 04/ml.py
+++ 04/ml.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
-# from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-from sklearn.datasets import fetch_california_housing
+# Load the dataset california_housing
 housing = fetch_california_housing()
 
-# Load the Boston Housing dataset
-# boston = load_boston()
+# Create a DataFrame from the dataset
 data = pd.DataFrame(housing.data, columns=housing.feature_names)
 data['PRICE'] = housing.target
 
